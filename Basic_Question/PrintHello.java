43ce3b123cc7f0c398cc87552326db32b3bba33b
RiyaRani14 <riya14042012@gmail.com>
2025-08-02 21:48:19 +0530

    Print Hello

---
+++ Basic_Question/PrintHello.java
@@ -0,0 +1,10 @@
+package Basic_Question;
+
+public class PrintHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
